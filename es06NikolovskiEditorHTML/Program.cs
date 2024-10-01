4408e0d4f4b49fdfd7fe60df586e0fc55cbfa94c
NikolovskiKristijan <k.nikolovski.3028@vallauri.edu>
2024-10-01 10:12:47 +0200

    Rinominato form

--- es06NikolovskiEditorHTML/Program.cs
+++ es06NikolovskiEditorHTML/Program.cs
@@ -16,7 +16,7 @@ namespace es06NikolovskiEditorHTML
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmEditorHTML());
         }
     }
 }
